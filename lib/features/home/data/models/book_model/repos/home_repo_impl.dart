8352c5f8f09dd54a5e659132a52a99a83e46b1d6
yousefsamir <yousefsamir4000@gmail.com>
2026-07-30 16:41:50 +0300

    implement fetch newest books

--- lib/features/home/data/models/book_model/repos/home_repo_impl.dart
+++ lib/features/home/data/models/book_model/repos/home_repo_impl.dart
@@ -1,13 +1,29 @@
 import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/features/home/data/models/book_model/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBox() {
-    // TODO: implement fetchBestSellerBox
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewestBox() async {
+    try {
+      var date = await apiService.get(
+        endpoint:
+            'volumes?q=subject:programming&key=AIzaSyAeijT38HZXJ1E-pdujo1FUoZ10RO8aXNY',
+      );
+
+      List<BookModel> books = [];
+      for (var item in date['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
